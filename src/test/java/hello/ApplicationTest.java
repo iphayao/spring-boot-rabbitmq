d986d8bc6b2db47f2bb0c5c436edb88e4ede04e5
iphayao <phayaob@gmail.com>
2018-06-30 15:09:38 +0700

    Add test

---
+++ src/test/java/hello/ApplicationTest.java
@@ -0,0 +1,30 @@
+package hello;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+    @MockBean
+    private Runner runner;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private Receiver receiver;
+
+    @Test
+    public void test() throws InterruptedException {
+        rabbitTemplate.convertAndSend(Application.queueName, "Hello form RabbitMQ!");
+        receiver.getLatch().await(1000, TimeUnit.MILLISECONDS);
+    }
+}
